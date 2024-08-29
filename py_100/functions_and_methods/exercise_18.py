057b775da4fe9d3d57d4ad25487dcb002530ba37
Caroline Wood <carolinewood72@gmail.com>
2024-08-29 12:55:28 +0100

    modified file

--- py_100/functions_and_methods/exercise_18.py
+++ py_100/functions_and_methods/exercise_18.py
@@ -5,12 +5,18 @@ def remainders_3(numbers):
 
 # Use this function to determine which of the following lists contains at least one number that is not evenly divisible by 3 (that is, the remainder is not 0):
 
-numbers_1 = [0, 1, 2, 3, 4, 5, 6]   # True
-numbers_2 = [1, 2, 4, 5]            # True
-numbers_3 = [0, 3, 6]               # False
-numbers_4 = []                      # False
+numbers_1 = [0, 1, 2, 3, 4, 5, 6]   # True  [0, 1, 2, 0, 1, 2, 0]
+numbers_2 = [1, 2, 4, 5]            # True  [1, 2, 1, 2]
+numbers_3 = [0, 3, 6]               # False [0, 0, 0]
+numbers_4 = []                      # False []
 
-print(any(remainders_3(numbers_1))) # True
-print(any(remainders_3(numbers_2))) # True
-print(any(remainders_3(numbers_3))) # False
-print(any(remainders_3(numbers_4))) # False
+# print(remainders_3(numbers_1))   # [0, 1, 2, 0, 1, 2, 0]
+# print(remainders_3(numbers_2))   # [1, 2, 1, 2]
+# print(remainders_3(numbers_3))   # [0, 0, 0]
+# print(remainders_3(numbers_4))   # []
+
+
+# print(any(remainders_3(numbers_1))) # True
+# print(any(remainders_3(numbers_2))) # True
+# print(any(remainders_3(numbers_3))) # False
+# print(any(remainders_3(numbers_4))) # False
---
+++ py_100/functions_and_methods/exercise_19.py
@@ -0,0 +1,26 @@
+# The following function returns a list of the remainders 
+# of dividing the numbers in numbers by 5:
+def remainders_5(numbers):
+    return [number % 5 for number in numbers]
+
+
+# Use this function to determine which of the following 
+# lists do not contain any numbers that are divisible by 5:
+
+numbers_1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]  # False
+numbers_2 = [1, 2, 3, 4, 6, 7, 8, 9]            # True
+numbers_3 = [0, 5, 10]                          # False
+numbers_4 = []                                  # True
+
+# numbers_1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]   # [0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0]
+
+# numbers_2 = [1, 2, 3, 4, 6, 7, 8, 9]             # [1, 2, 3, 4, 1, 2, 3, 4]
+
+# numbers_3 = [0, 5, 10]                           # [0, 0, 0]
+
+# numbers_4 = []                                   # []
+
+print(all(remainders_5(numbers_1)))
+print(all(remainders_5(numbers_2)))
+print(all(remainders_5(numbers_3)))
+print(all(remainders_5(numbers_4)))
