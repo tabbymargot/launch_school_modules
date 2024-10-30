854549b9774a151ce326f01db3c2f7199d0c4dba
Caroline Wood <carolinewood72@gmail.com>
2024-10-30 12:59:51 +0000

    replaced if / else with match / case

--- py_101/py_101_lesson_2_small_programs/calculator.py
+++ py_101/py_101_lesson_2_small_programs/calculator.py
@@ -12,13 +12,15 @@ number2 = input()
 prompt('What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide')
 operation = input()
 
-if operation == '1':   # '1' represents addition
-    output = int(number1) + int(number2)
-elif operation == '2': # '2' represents subtraction
-    output = int(number1) - int(number2)
-elif operation == '3': # '3' represents multiplication
-    output = int(number1) * int(number2)
-elif operation == '4': # '4' represents division
-    output = int(number1) / int(number2)
+match operation:
+
+    case '1':   # '1' represents addition
+        output = int(number1) + int(number2)
+    case '2': # '2' represents subtraction
+        output = int(number1) - int(number2)
+    case '3': # '3' represents multiplication
+        output = int(number1) * int(number2)
+    case '4': # '4' represents division
+        output = int(number1) / int(number2)
 
 prompt(f"The result is: {output}")
