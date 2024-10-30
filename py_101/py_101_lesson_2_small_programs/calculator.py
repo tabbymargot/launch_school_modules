e82cd663b5d00161cca36c10841e341f12b61cc4
Caroline Wood <carolinewood72@gmail.com>
2024-10-30 12:54:50 +0000

    added prompt function

--- py_101/py_101_lesson_2_small_programs/calculator.py
+++ py_101/py_101_lesson_2_small_programs/calculator.py
@@ -1,12 +1,15 @@
-print('Welcome to Calculator!')
+def prompt(message):
+    print(f'==> {message}')
 
-print("What's the first number?")
+prompt('Welcome to Calculator!')
+
+prompt("What's the first number?")
 number1 = input()
 
-print("What's the second number?")
+prompt("What's the second number?")
 number2 = input()
 
-print('What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide')
+prompt('What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide')
 operation = input()
 
 if operation == '1':   # '1' represents addition
@@ -18,4 +21,4 @@ elif operation == '3': # '3' represents multiplication
 elif operation == '4': # '4' represents division
     output = int(number1) / int(number2)
 
-print(f"The result is: {output}")
+prompt(f"The result is: {output}")
