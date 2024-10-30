8fb3eb272525b4fed0dbec3f44494b20e6a07e6a
Caroline Wood <carolinewood72@gmail.com>
2024-10-30 15:19:11 +0000

    added code to validate inputs

--- py_101/py_101_lesson_2_small_programs/calculator.py
+++ py_101/py_101_lesson_2_small_programs/calculator.py
@@ -1,16 +1,36 @@
 def prompt(message):
     print(f'==> {message}')
 
+def invalid_number(number_str):
+    try:
+        int(number_str)
+    except ValueError:
+        return True
+    
+    return False
+
 prompt('Welcome to Calculator!')
 
 prompt("What's the first number?")
 number1 = input()
 
+while invalid_number(number1): # Returns True if number is NOT valid.
+    prompt("Hmm... that doesn't look like a valid number.")
+    number1 = input()
+
 prompt("What's the second number?")
 number2 = input()
 
+while invalid_number(number2): # Returns True if number is NOT valid.
+    prompt("Hmm... that doesn't look like a valid number.")
+    number2 = input() 
+
 prompt('What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide')
 operation = input()
+
+while operation not in ['1', '2', '3', '4']:
+    prompt('You must choose 1, 2, 3 or 4')
+    operation = input()
 
 match operation:
 
