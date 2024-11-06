ebe0e8794b77e4324183be35668507bc056d4da8
Caroline Wood <carolinewood72@gmail.com>
2024-11-06 14:31:59 +0000

    deleted blank lines

--- py_101/py_101_lesson_2_small_programs/rock_paper_scissors.py
+++ py_101/py_101_lesson_2_small_programs/rock_paper_scissors.py
@@ -1,22 +1,6 @@
 import random
 
-VALID_CHOICES = ["rock", "paper", "scissors"]
-
-
-
-def display_winner(player, computer):
-    prompt(f"You chose {player}, computer chose {computer}")
-
-    if ((player == "rock" and computer == "scissors") or
-        (player == "paper" and computer == "rock") or
-        (player == "scissors" and computer == "paper")):
-        prompt("You win!")
-    elif ((player == "rock" and computer == "paper") or
-          (player == "paper" and computer == "scissors") or
-          (player == "scissors" and computer == "rock")):
-        prompt("Computer wins!")
-    else:
-        prompt("It's a tie!")
+VALID_CHOICES = ['rock', 'paper', 'scissors']
 
 def prompt(message):
     print(f"==> {message}")
@@ -31,13 +15,27 @@ while True:
 
     computer_choice = random.choice(VALID_CHOICES)
 
-    display_winner(choice, computer_choice)
+    prompt(f"You chose {choice}, computer chose {computer_choice}")
 
-    prompt("Do you want to play again (y/n)?")
-    answer = input().lower()
+    if ((choice == "rock" and computer_choice == "scissors") or
+        (choice == "paper" and computer_choice == "rock") or
+        (choice == "scissors" and computer_choice == "paper")):
+        prompt("You win!")
+    elif ((choice == "rock" and computer_choice == "paper") or
+          (choice == "paper" and computer_choice == "scissors") or
+          (choice == "scissors" and computer_choice == "rock")):
+        prompt("Computer wins!")
+    else:
+        prompt("It's a tie!")
 
-    while answer.startswith('n') == False and answer.startswith('y') == False:
-        prompt("That's not a valid choice")
+    while True:
+        prompt("Do you want to play again (y/n)?")
+        answer = input().lower()
+
+        if answer.startswith('n') or answer.startswith('y'):
+            break
+        else:
+            prompt("That's not a valid choice")
 
     if answer[0] == 'n':
         break
