c27a818cefa8babb7ea8fe0ede175fa3e3c5bd6a
Caroline Wood <carolinewood72@gmail.com>
2024-11-07 11:41:26 +0000

    after running through Pylint

--- py_101/py_101_lesson_2_small_programs/rps_bonus.py
+++ py_101/py_101_lesson_2_small_programs/rps_bonus.py
@@ -5,39 +5,84 @@ VALID_CHOICES = ['rock', 'paper', 'scissors', 'spock', 'scissors']
 def prompt(message):
     print(f"==> {message}")
 
-# def calculate_overall_winner(player_score, computer_score):
-#     while player_score < 3 or computer_score < 3:
-#         player_score += player_score
-#         computer_score += computer_score
+def get_choice(word_choice):
+    while word_choice is None:
+        prompt("That's not a valid choice")
+        player_choice = input()
+        word_choice = abbreviations.get(player_choice)
 
-#     if player_score == 3:
-#         return player_score
-#     else:
-#         return computer_score 
+    return word_choice
 
-def display_winner():
-    if ((full_word_choice == "rock" and computer_choice == ("scissors" or "lizard")) or
-        (full_word_choice == "scissors" and computer_choice == ("paper" or "lizard")) or
-        (full_word_choice == "paper" and computer_choice == ("rock" or "spock")) or
-        (full_word_choice == "spock" and computer_choice == ("scissors" or "rock")) or
-        (full_word_choice == "lizard" and computer_choice == ("paper" or "spock"))):
-        prompt("You win!")
-    elif ((full_word_choice == "rock" and computer_choice == ("paper" or "spock")) or
-        (full_word_choice == "paper" and computer_choice == ("scissors" or "lizard")) or
-        (full_word_choice == "scissors" and computer_choice == ("rock" or "spock")) or
-        (full_word_choice == "spock" and computer_choice == ("lizard" or "paper")) or
-        (full_word_choice == "scissors" and computer_choice == ("rock" or "spock"))):
-        prompt("Computer wins!")
+def compute_game_winner():
+    if (((full_word_choice == "rock" and
+         computer_choice == ("scissors" or "lizard"))) or
+        ((full_word_choice == "scissors" and
+        computer_choice == ("paper" or "lizard"))) or
+        ((full_word_choice == "paper" and
+        computer_choice == ("rock" or "spock"))) or
+        ((full_word_choice == "spock" and
+        computer_choice == ("scissors" or "rock"))) or
+        ((full_word_choice == "lizard" and
+        computer_choice == ("paper" or "spock")))):
+        return 'player_wins'
+
+    if (((full_word_choice == "rock" and
+        computer_choice == ("paper" or "spock"))) or
+        ((full_word_choice == "paper" and
+        computer_choice == ("scissors" or "lizard"))) or
+        ((full_word_choice == "scissors" and
+        computer_choice == ("rock" or "spock"))) or
+        ((full_word_choice == "spock" and
+        computer_choice == ("lizard" or "paper"))) or
+        ((full_word_choice == "scissors" and
+        computer_choice == ("rock" or "spock")))):
+        return 'computer_wins'
+
+    return 'tie'
+
+def display_game_winner(the_result):
+    if the_result == 'player_wins':
+        prompt('You win!\n')
+    elif the_result == 'computer_wins':
+        prompt('Computer wins!\n')
     else:
-        prompt("It's a tie!")
+        prompt("It's a tie.\n")
 
-# def calculate_winner():
+def increment_score(the_result, player_score, computer_score):
+    if the_result == 'player_wins':
+        player_score += 1
+    elif the_result == 'computer_wins':
+        computer_score += 1
 
-player_score = 0
-computer_score = 0
-answer = ''
+    return player_score, computer_score
 
-prompt(f'Welcome to Best of Five!')
+def display_scores(player_score, computer_score):
+    prompt(f"Your score is {player_score}.")
+    prompt(f"The computer's score is {computer_score}.\n")
+
+def display_grand_winner(player_score, computer_score):
+    if player_score == 3:
+        prompt('You are the grand winner!')
+    elif computer_score == 3:
+        prompt('The computer is the grand winner!')
+    else:
+        pass
+
+def get_continuation_preference():
+    while player_current_score == 3 or computer_current_score == 3:
+        prompt("Do you want to play again (y/n)?")
+        answer = input().lower()
+
+        if answer.startswith('n') or answer.startswith('y'):
+            return answer
+        else:
+            prompt("That's not a valid choice")
+
+player_current_score = 0
+computer_current_score = 0
+continuation_preference = ''
+
+prompt('Welcome to Best of Five!')
 
 while True:
 
@@ -54,56 +99,33 @@ while True:
 
     full_word_choice = abbreviations.get(choice)
 
-    while full_word_choice == None:
-        prompt("That's not a valid choice")
-        choice = input()
-        full_word_choice = abbreviations.get(choice)
+    validated_choice = get_choice(full_word_choice)
 
     computer_choice = random.choice(VALID_CHOICES)
 
-    prompt(f"You chose {full_word_choice}, computer chose {computer_choice}")
+    prompt(f"You chose {validated_choice}, computer chose {computer_choice}")
 
-    display_winner()
+    result = compute_game_winner()
 
-    if ((full_word_choice == "rock" and computer_choice == ("scissors" or "lizard")) or
-        (full_word_choice == "scissors" and computer_choice == ("paper" or "lizard")) or
-        (full_word_choice == "paper" and computer_choice == ("rock" or "spock")) or
-        (full_word_choice == "spock" and computer_choice == ("scissors" or "rock")) or
-        (full_word_choice == "lizard" and computer_choice == ("paper" or "spock"))):
+    display_game_winner(result)
 
-        player_score += 1
-        print(f'player score: {player_score}')
+    player_current_score, computer_current_score =  (increment_score
+                                                    (result,
+                                                    player_current_score,
+                                                    computer_current_score))
 
-    elif ((full_word_choice == "rock" and computer_choice == ("paper" or "spock")) or
-        (full_word_choice == "paper" and computer_choice == ("scissors" or "lizard")) or
-        (full_word_choice == "scissors" and computer_choice == ("rock" or "spock")) or
-        (full_word_choice == "spock" and computer_choice == ("lizard" or "paper")) or
-        (full_word_choice == "scissors" and computer_choice == ("rock" or "spock"))):
+    display_scores(player_current_score, computer_current_score)
 
-        computer_score += 1
-        print(f'computer score: {computer_score}')
+    display_grand_winner(player_current_score, computer_current_score)
 
-    if player_score == 3:
-        prompt('You are the overall winner!')
-    elif computer_score == 3:
-        prompt('The computer is the overall winner!')
-    else:
-        pass
+    continuation_preference = get_continuation_preference()
 
-    while player_score == 3 or computer_score == 3:
-        prompt("Do you want to play again (y/n)?")
-        answer = input().lower()
-
-        if answer.startswith('n') or answer.startswith('y'):
+    match continuation_preference:
+        case None:
+            pass # Best of Five game not yet finished; continue looping.
+        case 'n':
             break
-        else:
-            prompt("That's not a valid choice")
-
-    if answer == '':
-        pass
-    elif answer[0] == 'n':
-        break
-    elif answer[0] == 'y':
-        player_score = 0
-        computer_score = 0
-        answer = ''
+        case 'y':
+            player_current_score = 0
+            computer_current_score = 0
+            continuation_preference = ''
