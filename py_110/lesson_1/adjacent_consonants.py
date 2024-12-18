063b79982d3b2b94fa630a29d640106283715013
Caroline Wood <carolinewood72@gmail.com>
2024-12-18 11:21:24 +0000

    Problem solved

--- py_110/lesson_1/adjacent_consonants.py
+++ py_110/lesson_1/adjacent_consonants.py
@@ -7,59 +7,68 @@ def sort_by_consonant_count(lst):
 
     for word in lst:
         points_running_total = 0
-        word = word.replace(" ", "")
+        word_no_space = word.replace(" ", "")
 
-        for idx, current_letter in enumerate(word):
+        for idx, current_letter in enumerate(word_no_space):
 
-            if idx != len(word) - 1:
-                next_letter = word[idx + 1]
+            if idx != len(word_no_space) - 1:
+                next_letter = word_no_space[idx + 1]
             if idx != 0:
-                previous_letter = word[idx - 1]
+                previous_letter = word_no_space[idx - 1]
 
             if current_letter in CONSONANTS:
                 # If it's the first letter in the string and the next letter is a consonant
                 if idx == 0:
-                    # print(current_letter)
                     if next_letter in CONSONANTS:
                         points_running_total += 1
-                        # print(points_running_total)
                 # If it's the last letter in the string and the previous letter is a consonant
-                elif idx == len(word) - 1:
-                    # print(current_letter)
+                elif idx == len(word_no_space) - 1:
                     if previous_letter in CONSONANTS:
                         points_running_total += 1
-                        # print(points_running_total)
                 # If it's any other letter in the string and the previous and / or next letter is a consonant
                 else:
-                    # print(current_letter)
                     if (previous_letter in CONSONANTS) or (next_letter in CONSONANTS):
                         points_running_total += 1
-                        # print(points_running_total)
+        
         word_and_score = [word, points_running_total]  
-        # print(word_and_score)    
         words_and_scores.append(word_and_score)
     
-    return words_and_scores 
+    list_of_sorted_words = sort_the_words(words_and_scores)
+    return list_of_sorted_words
 
+def sort_the_words(the_words_and_scores):
+    scores = []
+    sorted_words = []
 
-# my_list = ['bbaa', 'ccaa']
-# print(sort_by_consonant_count(my_list))
+    for nested_list in the_words_and_scores:
+        scores.append(nested_list[1])
 
-# my_list = ['aa', 'baa', 'ccaa', 'dddaa']
-# print(sort_by_consonant_count(my_list))
+    score_being_checked = max(scores)
+
+    while score_being_checked >= 0:
+        for the_nested_list in the_words_and_scores:
+            if the_nested_list[1] == score_being_checked:
+                sorted_words.append(the_nested_list[0])
+
+        score_being_checked -= 1
+
+    return sorted_words
+
+my_list = ['aa', 'baa', 'ccaa', 'dddaa']
+print(sort_by_consonant_count(my_list))
 # # ['dddaa', 'ccaa', 'aa', 'baa']
 
-# my_list = ['can can', 'toucan', 'batman', 'salt pan']
-# print(sort_by_consonant_count(my_list))
+my_list = ['can can', 'toucan', 'batman', 'salt pan']
+print(sort_by_consonant_count(my_list))
 # # ['salt pan', 'can can', 'batman', 'toucan']
 
 my_list = ['bar', 'car', 'far', 'jar']
 print(sort_by_consonant_count(my_list))
-# # ['bar', 'car', 'far', 'jar']
+# # # ['bar', 'car', 'far', 'jar']
 
 my_list = ['day', 'week', 'month', 'year']
 print(sort_by_consonant_count(my_list))
-# # ['month', 'day', 'week', 'year']
+# # # ['month', 'day', 'week', 'year']
 
 my_list = ['xxxa', 'xxxx', 'xxxb']
 print(sort_by_consonant_count(my_list))
