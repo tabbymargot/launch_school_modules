ba8ab3a1f7c392536a3938984f8ea09bfa4ff7e8
Caroline Wood <carolinewood72@gmail.com>
2024-12-18 10:51:37 +0000

    Made some corrections and added step 2: append the word and its score to store_nested_lists

--- py_110/lesson_1/adjacent_consonants.py
+++ py_110/lesson_1/adjacent_consonants.py
@@ -1,11 +1,12 @@
 CONSONANTS = "bcdfghjklmnpqrstvwxyz"
 
 def sort_by_consonant_count(lst):
-    points_running_total = 0
-    store_nested_lists = []
+    
+    words_and_scores = []
     sorted_words = []
 
     for word in lst:
+        points_running_total = 0
         word = word.replace(" ", "")
 
         for idx, current_letter in enumerate(word):
@@ -18,25 +19,31 @@ def sort_by_consonant_count(lst):
             if current_letter in CONSONANTS:
                 # If it's the first letter in the string and the next letter is a consonant
                 if idx == 0:
-                    print(current_letter)
+                    # print(current_letter)
                     if next_letter in CONSONANTS:
                         points_running_total += 1
-                        print(points_running_total)
+                        # print(points_running_total)
                 # If it's the last letter in the string and the previous letter is a consonant
                 elif idx == len(word) - 1:
-                    print(current_letter)
+                    # print(current_letter)
                     if previous_letter in CONSONANTS:
                         points_running_total += 1
-                        print(points_running_total)
+                        # print(points_running_total)
                 # If it's any other letter in the string and the previous and / or next letter is a consonant
                 else:
-                    print(current_letter)
+                    # print(current_letter)
                     if (previous_letter in CONSONANTS) or (next_letter in CONSONANTS):
                         points_running_total += 1
-                        print(points_running_total)
+                        # print(points_running_total)
+        word_and_score = [word, points_running_total]  
+        # print(word_and_score)    
+        words_and_scores.append(word_and_score)
+    
+    return words_and_scores 
 
-my_list = ['rrr']
-print(sort_by_consonant_count(my_list))
+
+# my_list = ['bbaa', 'ccaa']
+# print(sort_by_consonant_count(my_list))
 
 # my_list = ['aa', 'baa', 'ccaa', 'dddaa']
 # print(sort_by_consonant_count(my_list))
@@ -46,14 +53,14 @@ print(sort_by_consonant_count(my_list))
 # print(sort_by_consonant_count(my_list))
 # # ['salt pan', 'can can', 'batman', 'toucan']
 
-# my_list = ['bar', 'car', 'far', 'jar']
-# print(sort_by_consonant_count(my_list))
+my_list = ['bar', 'car', 'far', 'jar']
+print(sort_by_consonant_count(my_list))
 # # ['bar', 'car', 'far', 'jar']
 
-# my_list = ['day', 'week', 'month', 'year']
-# print(sort_by_consonant_count(my_list))
+my_list = ['day', 'week', 'month', 'year']
+print(sort_by_consonant_count(my_list))
 # # ['month', 'day', 'week', 'year']
 
-# my_list = ['xxxa', 'xxxx', 'xxxb']
-# print(sort_by_consonant_count(my_list))
+my_list = ['xxxa', 'xxxx', 'xxxb']
+print(sort_by_consonant_count(my_list))
 # # ['xxxx', 'xxxb', 'xxxa']
