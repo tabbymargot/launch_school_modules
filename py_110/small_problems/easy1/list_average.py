83f3b479575645bf4774b46f1ee2d4cb11958d31
Caroline Wood <carolinewood72@gmail.com>
2025-01-23 12:42:29 +0000

    my solution

---
+++ py_110/small_problems/easy1/list_average.py
@@ -0,0 +1,6 @@
+def average(num_list):
+    return sum(num_list) // len(num_list)
+
+print(average([1, 5, 87, 45, 8, 8]) == 25)        # True
+print(average([9, 47, 23, 95, 16, 52]) == 40)     # True
+print(average([7]) == 7)                          # True
