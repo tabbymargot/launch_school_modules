7030a12f60a72a8bd1debd77cb6d32c0351102b4
Caroline Wood <carolinewood72@gmail.com>
2025-01-28 14:41:43 +0000

    my solution

---
+++ py_110/small_problems/easy1/retain_specific_keys.py
@@ -0,0 +1,17 @@
+def keep_keys(input_dict, keys):
+    return {key: value 
+            for key, value in input_dict.items()
+            if key in keys}
+
+
+
+input_dict = {
+    'red': 1,
+    'green': 2,
+    'blue': 3,
+    'yellow': 4,
+}
+
+keys = ['red', 'blue']
+expected_dict = {'red': 1, 'blue': 3}
+print(keep_keys(input_dict, keys) == expected_dict) # True
