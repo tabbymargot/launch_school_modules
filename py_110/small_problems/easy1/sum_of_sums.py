3fe12c7dd71ba4aea36c326b4ef4c3f7d2ec40ea
Caroline Wood <carolinewood72@gmail.com>
2025-01-29 14:42:58 +0000

    my solution

---
+++ py_110/small_problems/easy1/sum_of_sums.py
@@ -0,0 +1,18 @@
+def sum_of_sums(num_list):
+    running_total = 0
+
+    for idx, num in enumerate(num_list):
+        running_total += sum(num_list[:idx + 1])
+    
+    return running_total
+
+print(sum_of_sums([3, 5, 2]) == 21)               # True
+# (3) + (3 + 5) + (3 + 5 + 2) --> 21
+
+print(sum_of_sums([1, 5, 7, 3]) == 36)            # True
+# (1) + (1 + 5) + (1 + 5 + 7) + (1 + 5 + 7 + 3) --> 36
+
+print(sum_of_sums([1, 2, 3, 4, 5]) == 35)         # True
+# (1) + (1+2) + (1+2+3) + (1+2+3+4) + (1+2+3+4+5) --> 35
+
+print(sum_of_sums([4]) == 4)                      # True
