3066fc5cb6fd2a529fd9f05ae56a37995d5f16a8
Caroline Wood <carolinewood72@gmail.com>
2025-02-04 12:10:54 +0000

    fixed some typos

--- py_110/lesson_3/tictactoe_second_attempt.py
+++ py_110/lesson_3/tictactoe_second_attempt.py
@@ -220,15 +220,15 @@ def play_tic_tac_toe():
             else:
                 computer_score += 1
 
-            prompt(f"{winner} won!\n")
-            prompt(f'You\'re current score is {player_score}.\n')
+            prompt(f"The {winner} won!\n")
+            prompt(f'Your current score is {player_score}.\n')
             prompt(f'The computer\'s current score is {computer_score}.\n')
 
         else:
             prompt("It's a tie!\n")
 
         if GAMES_TO_WIN_MATCH in (player_score, computer_score):
-            prompt(f'{winner} wins the match! \U0001F3C6 \n')
+            prompt(f'The {winner} wins the match! \U0001F3C6 \n')
             player_score = 0
             computer_score = 0
 
