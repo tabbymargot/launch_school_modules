ba9a7690913a0b596a185ae3767a23b3cac08ee8
Caroline Wood <carolinewood72@gmail.com>
2025-03-10 10:40:51 +0000

    modified to use anchor variable

--- py_110/small_problems/medium1/fibonacci_numbers_prodedural.py
+++ py_110/small_problems/medium1/fibonacci_numbers_prodedural.py
@@ -1,3 +1,12 @@
+# The Fibonacci series is a sequence of numbers in which each number is the sum of the previous two numbers. 
+# The first two Fibonacci numbers are 1 and 1. 
+# The third number is 1 + 1 = 2, the fourth is 1 + 2 = 3, the fifth is 2 + 3 = 5, the sixth is 3 + 5 = 8, and so on. 
+# In mathematical terms, this can be represented as:
+
+# F(1) = 1
+# F(2) = 1
+# F(n) = F(n - 1) + F(n - 2)    (where n > 2)
+
 """  
 PROBLEM
 Restate in my words
@@ -34,46 +43,46 @@ def fibonacci(n):
     
     # Return the nth number
 
-
-
-
 Alternative High Level?
 Actual algorithm
-
-
 """
 
 """ 
 Sequence: 1, 1, 2, 3, 5, 8
 
+Anchor is the last index position that we want to access.
+
 - F3
     - Add the numbers at indexes 0 (1) + 1 (1), and append result to list (2)
         [1, 1, 2]
+    - Anchor = 1 (3 - 2)
 - F4
     - Add the numbers at indexes 0 (1) + 1 (1), and append result to list (2)
     - Add the numbers at indexes 1 (1) + 2 (2), and append result to list (3)
         [1, 1, 2, 3]
+    - Anchor = 2 (4 - 2)
 - F5
     - Add the numbers at indexes 0 (1) + 1 (1), and append result to list (2)
     - Add the numbers at indexes 1 (1) + 2 (2), and append result to list (3)
     - Add the numbers at indexes 2 (2) + 3 (3), and append result to list (5)
         [1, 1, 2, 3, 5]
+    - Anchor = 3 (5 - 2)
 - F6
     - Add the numbers at indexes 0 (1) + 1 (1), and append result to list (2)
     - Add the numbers at indexes 1 (1) + 2 (2), and append result to list (3)
     - Add the numbers at indexes 2 (2) + 3 (3), and append result to list (5)
     - Add the numbers at indexes 3 (3) + 4 (5), and append result to list (8)
         [1, 1, 2, 3, 5, 8] 
+    - Anchor = 4 (6 - 2)
 """
 
-
-def fibonacci(n):
+def fibonacci(num):
     fib_list = [1, 1]
-    for idx in range(n - 2):
-        fib_list.append(fib_list[idx] + fib_list[idx + 1])
+    anchor = num - 2 
+    for idx in range(anchor): 
+        fib_list.append(fib_list[idx]+ fib_list[idx + 1])
     
     return fib_list[-1]
-
 
 
 print(fibonacci(1) == 1)                  # True
