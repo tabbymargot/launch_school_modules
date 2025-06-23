9f30190ea21993da849c65ab61bf6f730964710e
Caroline Wood <carolinewood72@gmail.com>
2025-06-23 16:47:33 +0100

    initial pylint

--- py_120/lesson_2_oop/oo_21.py
+++ py_120/lesson_2_oop/oo_21.py
@@ -9,9 +9,10 @@ import random
 import time
 
 MAX_WINNING_SCORE = 21
-SUITS = ('Clubs', 'Diamonds', 'Hearts', 'Spades')
-STR_VALUES = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
-SCORES = ((1, 11), 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10)
+SUITS             = ('Clubs', 'Diamonds', 'Hearts', 'Spades')
+STR_VALUES        = (['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10',
+                    'Jack', 'Queen', 'King'])
+SCORES            =  ((1, 11), 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10)
 
 class Card:
     def __init__(self):
@@ -24,34 +25,37 @@ class Card:
     @property
     def suit(self):
         return self._suit
-    
+
     @suit.setter
     def suit(self, suit):
         if not self._initializing and suit not in SUITS:
-            raise ValueError("Suit must be one of the following strings: Clubs, Diamonds, Hearts or Spades")
+            raise ValueError(
+                "Suit must be one of the following strings: " \
+                "Clubs, Diamonds, Hearts or Spades"
+                )
 
         self._suit = suit
 
     @property
     def str_value(self):
         return self._str_value
-    
+
     @str_value.setter
     def str_value(self, str_value):
         if not self._initializing and str_value not in STR_VALUES:
             raise ValueError("Invalid string value")
-        
+
         self._str_value = str_value
 
     @property
     def score(self):
         return self._score
-    
+
     @score.setter
     def score(self, score):
         if not self._initializing and score not in SCORES:
             raise ValueError("Invalid card score.")
-        
+
         self._score = score
 
 class Hand():
@@ -66,47 +70,53 @@ class Hand():
     @property
     def cards(self):
         return self._cards
-    
+
     @cards.setter
     def cards(self, cards):
         if not self._initializing and not isinstance(cards, list):
             raise TypeError("The hand's card attribute must be a list")
-        
+
         self._cards = cards
 
     @property
     def score(self):
         return self._score
-    
+
     @score.setter
     def score(self, score):
         # if not self._initializing and not isinstance(score, int):
         #     raise TypeError("The hand's score attribute must be an integer")
         if not isinstance(score, int):
             raise TypeError("The hand's score attribute must be an integer")
-        
+
         self._score = score
 
     @property
     def all_cards_except_last(self):
         return self._all_cards_except_last
-    
+
     @all_cards_except_last.setter
     def all_cards_except_last(self, all_cards_except_last):
-        if not self._initializing and not isinstance(all_cards_except_last, list):
-            raise TypeError("The hand's all_cards_except_last attribute must be a list")
-        
+        if  not self._initializing and \
+            not isinstance(all_cards_except_last, list):
+            raise TypeError(
+                "The hand's all_cards_except_last attribute must be a list"
+                )
+
         self._all_cards_except_last = all_cards_except_last
 
     @property
     def most_recently_dealt_card(self):
         return self._most_recently_dealt_card
-    
+
     @most_recently_dealt_card.setter
     def most_recently_dealt_card(self, most_recently_dealt_card):
-        if not self._initializing and not isinstance(most_recently_dealt_card, Card):
-            raise TypeError("The hand's most_recently_dealt_card attribute must be a Card")
-        
+        if  not self._initializing and \
+            not isinstance(most_recently_dealt_card, Card):
+            raise TypeError(
+                "The hand's most_recently_dealt_card attribute must be a Card"
+                )
+
         self._most_recently_dealt_card = most_recently_dealt_card
 
     def calculate_value(self):
@@ -124,7 +134,7 @@ class Hand():
         aces_score = self.calculate_ace_values(aces, non_aces_score)
 
         self.score = non_aces_score + aces_score
-    
+
     def calculate_ace_values(self, aces, non_aces_score):
         aces_values = []
 
@@ -152,16 +162,35 @@ class Hand():
                 pass
 
         return sum(aces_values)
-    
+
     def get_details_of_all_cards_except_last(self):
-        card_details = []
+        all_card_details = []
+
+        # print(f"DDD: {list(self.all_cards_except_last)}")
+
+        print(f'Type: {type(self.all_cards_except_last)}')
 
         for card in self.all_cards_except_last:
             suit = card.suit
             value = card.str_value
-            card_details.append(f'the {value} of {suit}')
+            all_card_details.append(f'the {value} of {suit}')
 
-        return ', '.join([details for details in card_details])
+        # if len(self.all_cards_except_last) == 1:
+        #     card = self.all_cards_except_last[0]
+        #     value, suit = self.get_card_values(card)
+        #     all_card_details.append(f'the {value} of {suit}')
+        # else:
+        #     for card in self.all_cards_except_last:
+        #         value, suit = self.get_card_values(card)
+        #         all_card_details.append(f'the {value} of {suit}')
+
+        return ', '.join(list(all_card_details))
+
+    # def get_card_values(self, card):
+    #     suit = card.suit
+    #     value = card.str_value
+
+    #     return value, suit
 
     def get_last_dealt_card_details(self):
         suit = self.most_recently_dealt_card.suit
@@ -187,13 +216,13 @@ class Deck:
     @property
     def cards(self):
         return self._cards
-    
+
     @cards.setter
     def cards(self, cards):
         if not isinstance(cards, list):
             raise TypeError("The deck's cards attribute must be a list")
-        
-        self._cards = cards 
+
+        self._cards = cards
 
 class Participant:
 
@@ -204,11 +233,11 @@ class Participant:
         # What else goes here? all the redundant behaviors
         #   from Player and Dealer?
         self.hand = Hand()
-    
+
     @property
     def hand(self):
         return self._hand
-    
+
     @hand.setter
     def hand(self, hand):
         self._hand = hand
@@ -236,7 +265,7 @@ class Player(Participant):
     @bankroll.setter
     def bankroll(self, new_bankroll):
         self._bankroll = new_bankroll
-    
+
 class Dealer(Participant):
 
     def __init__(self):
@@ -249,7 +278,7 @@ class Dealer(Participant):
     def calculate_number_of_cards_to_deal(self, participant):
         if len(participant.hand.cards) == 0:
             return 2
-        
+
         return 1
 
     def update_hand(self, participant, card):
@@ -261,17 +290,17 @@ class Dealer(Participant):
         random.shuffle(self._deck.cards)
 
         number_of_cards = self.calculate_number_of_cards_to_deal(participant)
-        
+
         for _ in range(number_of_cards):
             try:
                 card = self._deck.cards[0]
             except IndexError:
                 self.recreate_deck()
                 card = self._deck.cards[0]
-            
+
             self._deck.cards.remove(card)
             self.update_hand(participant, card)
-            
+
 class TwentyOneGame:
     DEALER_MINIMUM_SCORE = 17
     MIN_BANKROLL = 3
@@ -296,7 +325,7 @@ class TwentyOneGame:
         while True:
             if TwentyOneGame.games_played > 0:
                 self._player.create_empty_hand()
-                self._dealer.create_empty_hand()              
+                self._dealer.create_empty_hand()
 
             self._dealer.deal(self._player)
             self._dealer.deal(self._dealer)
@@ -333,16 +362,24 @@ class TwentyOneGame:
                 break
 
         self.display_goodbye_message()
-            
 
     def show_cards(self):
-        self.prompt(f"Your hand contains the {self._player.hand.get_details_of_all_cards_except_last()} and {self._player.hand.get_last_dealt_card_details()}.\n")
+        self.prompt(
+            f"Your hand contains the \
+            {self._player.hand.get_details_of_all_cards_except_last()} \
+            and {self._player.hand.get_last_dealt_card_details()}.\n"
+            )
         time.sleep(0.75)
 
-        self.prompt(f"Your hand is worth {self._player.hand.score} points.\n")
+        self.prompt(
+            f"Your hand is worth {self._player.hand.score} points.\n"
+            )
         time.sleep(0.75)
 
-        self.prompt(f"One of the dealer's two cards is {self._dealer.hand.get_last_dealt_card_details()}.\n")
+        self.prompt(
+            f"One of the dealer's two cards is \
+            {self._dealer.hand.get_last_dealt_card_details()}.\n"
+            )
         time.sleep(0.75)
 
     def player_turn(self):
@@ -380,14 +417,20 @@ class TwentyOneGame:
         self._player.hand.calculate_value()
 
     def print_updated_player_hand(self):
-        self.prompt(f"Your hand now contains the {self._player.hand.get_details_of_all_cards_except_last()} and {self._player.hand.get_last_dealt_card_details()}.\n")
+        self.prompt(
+            f"Your hand now contains the \
+            {self._player.hand.get_details_of_all_cards_except_last()} \
+            and {self._player.hand.get_last_dealt_card_details()}.\n"
+            )
 
     def print_updated_player_score(self):
         self.prompt(f'Your new score is {self._player.hand.score}.\n')
         # time.sleep(1)
 
-        self.prompt(f"As a reminder, one of the dealer's two " \
-                f"cards is {self._dealer.hand.get_last_dealt_card_details()}.\n")
+        self.prompt(
+            f"As a reminder, one of the dealer's two \
+            cards is {self._dealer.hand.get_last_dealt_card_details()}.\n"
+            )
         time.sleep(0.75)
 
     def dealer_turn(self):
@@ -398,12 +441,12 @@ class TwentyOneGame:
 
             if self._dealer.hand.score >= self.DEALER_MINIMUM_SCORE:
                 break
-            
+
             self.display_dealer_hand_info('Current score')
 
             self._dealer.deal(self._dealer)
             self._dealer.hand.calculate_value()
-            
+
             self.display_dealer_hand_info('Latest card, updated score')
 
             time.sleep(0.75)
@@ -411,12 +454,22 @@ class TwentyOneGame:
     def display_dealer_hand_info(self, required_info):
         match required_info:
             case 'Dealer hand':
-                self.prompt(f"The dealer's hand contains the {self._dealer.hand.get_details_of_all_cards_except_last()} and {self._dealer.hand.get_last_dealt_card_details()}.\n")
+                self.prompt(
+                    f"The dealer's hand contains the \
+                    {self._dealer.hand. \
+                    get_details_of_all_cards_except_last()} \
+                    and {self._dealer.hand.get_last_dealt_card_details()}.\n"
+                    )
             case 'Current score':
-                self.prompt(f"The dealer has {self._dealer.hand.score} points, so I'm just " \
-                "dealing them another card...\n") 
+                self.prompt(
+                    f"The dealer has {self._dealer.hand.score} points, so \
+                    I'm just dealing them another card...\n"
+                    )
             case 'Latest card, updated score':
-                self.prompt(f"The dealer's new card is {self._dealer.hand.get_last_dealt_card_details()}.\n")
+                self.prompt(
+                    f"The dealer's new card is \
+                    {self._dealer.hand.get_last_dealt_card_details()}.\n"
+                    )
 
     def display_goodbye_message(self):
         self.prompt("Thanks for playing 21! Goodbye!")
@@ -424,18 +477,18 @@ class TwentyOneGame:
     def establish_result(self):
         if self._player.hand.score > MAX_WINNING_SCORE:
             return 'player_bust'
-        
+
         if self._dealer.hand.score > MAX_WINNING_SCORE:
             return 'dealer_bust'
-        
+
         if self._player.hand.score > self._dealer.hand.score:
             return 'player_wins'
-        
+
         if self._dealer.hand.score > self._player.hand.score:
             return 'dealer_wins'
 
         return 'tie'
-    
+
     def update_player_bankroll(self, result):
         if result == 'tie':
             pass # Keep the bankroll as it is.
@@ -449,19 +502,25 @@ class TwentyOneGame:
             self.prompt("You've run out of funds. Bummer! \n")
 
         elif self._player.bankroll == self.MAX_BANKROLL:
-            self.prompt("You've got $10 in your bankroll! I can't afford to play with you anymore, so I'll have to end the game. \n ")
+            self.prompt(
+                "You've got $10 in your bankroll! I can't afford to play \
+                with you anymore, so I'll have to end the game. \n ")
 
     def display_result(self, result):
         player_score = self._player.hand.score
         dealer_score = self._dealer.hand.score
 
         if result != 'player_bust':
-            self.prompt(MESSAGES['both_scores'].format(player_score=player_score, dealer_score=dealer_score))
+            self.prompt(
+                MESSAGES['both_scores'].format \
+                (player_score=player_score, dealer_score=dealer_score)
+                )
             time.sleep(0.75)
 
         match result:
             case 'player_bust':
-                self.prompt(MESSAGES['new_score'].format(player_score=player_score))
+                self.prompt(MESSAGES['new_score'].format \
+                (player_score=player_score))
                 time.sleep(0.75)
 
                 self.prompt(MESSAGES['player_is_bust'])
@@ -485,7 +544,7 @@ class TwentyOneGame:
 
         self.prompt(f'Your bankroll is worth ${self._player.bankroll}.\n')
         time.sleep(0.75)
-    
+
     def get_player_intention(self):
         while True:
             self.prompt("Would you like to play again? Enter Y for yes " \
@@ -494,8 +553,8 @@ class TwentyOneGame:
 
             if play_again in ('y', 'n'):
                 break
-            else:
-                self.prompt("That's not a valid choice. Please try again\n.")
+
+            self.prompt("That's not a valid choice. Please try again\n.")
             time.sleep(0.75)
 
         return play_again
