964ea21c0428cf5ce6ca4061a826b406528e539b
Caroline Wood <carolinewood72@gmail.com>
2025-06-25 10:12:55 +0100

    second go at this exercise

---
+++ py_120/exercises/medium/circular_buffer_2.py
@@ -0,0 +1,58 @@
+class CircularBuffer:
+    def __init__(self, size):
+        self.size = size
+        self.container = []
+
+    def put(self, num):
+        if len(self.container) == self.size:
+            self.container.pop(0)
+
+        self.container.append(num)
+
+    def get(self):
+        if not self.container:
+            return None
+        
+        return self.container.pop(0)
+
+
+buffer = CircularBuffer(3)
+
+print(buffer.get() is None)          # True
+
+buffer.put(1)
+buffer.put(2)
+print(buffer.get() == 1)             # True
+
+buffer.put(3)
+buffer.put(4)
+print(buffer.get() == 2)             # True
+
+buffer.put(5)
+buffer.put(6)
+buffer.put(7)
+print(buffer.get() == 5)             # True
+print(buffer.get() == 6)             # True
+print(buffer.get() == 7)             # True
+print(buffer.get() is None)          # True
+
+buffer2 = CircularBuffer(4)
+
+print(buffer2.get() is None)         # True
+
+buffer2.put(1)
+buffer2.put(2)
+print(buffer2.get() == 1)            # True
+
+buffer2.put(3)
+buffer2.put(4)
+print(buffer2.get() == 2)            # True
+
+buffer2.put(5)
+buffer2.put(6)
+buffer2.put(7)
+print(buffer2.get() == 4)            # True
+print(buffer2.get() == 5)            # True
+print(buffer2.get() == 6)            # True
+print(buffer2.get() == 7)            # True
+print(buffer2.get() is None)         # True
